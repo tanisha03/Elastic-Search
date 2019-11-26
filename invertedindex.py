1075bdc3551b42ccea48977516d5ab538227109b
Tanisha <tanisha031199@gmail.com>
2019-11-26 21:42:37 +0530

    added

---
+++ invertedindex.py
@@ -0,0 +1,57 @@
+from apperance import Appearance
+import re
+
+
+class InvertedIndex:
+    """
+    Inverted Index class.
+    """
+
+    def __init__(self, db):
+        self.index = dict()
+        self.db = db
+
+    def __repr__(self):
+        """
+        String representation of the Database object
+        """
+        return str(self.index)
+
+    def index_document(self, document):
+        """
+        Process a given document, save it to the DB and update the index.
+        """
+
+        # Remove punctuation from the text.
+        clean_text = re.sub(r"[^\w\s]", "", document["text"])
+        terms = clean_text.split(" ")
+        appearances_dict = dict()
+        # Dictionary with each term and the frequency it appears in the text.
+        for term in terms:
+            term_frequency = (
+                appearances_dict[term].frequency if term in appearances_dict else 0
+            )
+            appearances_dict[term] = Appearance(document["id"], term_frequency + 1)
+
+        # Update the inverted index
+        update_dict = {
+            key: [appearance]
+            if key not in self.index
+            else self.index[key] + [appearance]
+            for (key, appearance) in appearances_dict.items()
+        }
+        self.index.update(update_dict)
+        # Add the document into the database
+        self.db.add(document)
+        return document
+
+    def lookup_query(self, query):
+        """
+        Returns the dictionary of terms with their correspondent Appearances. 
+        This is a very naive search since it will just split the terms and show
+        the documents where they appear.
+        """
+        return {
+            term: self.index[term] for term in query.split(" ") if term in self.index
+        }
+
