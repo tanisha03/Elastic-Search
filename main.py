9798dfcde2d0524ce7ca2c6e39437b5b06754888
Tanisha <tanisha031199@gmail.com>
2019-11-26 21:51:06 +0530

    added

--- main.py
+++ main.py
@@ -2,13 +2,6 @@ from database import Database
 from invertedindex import InvertedIndex
 
 
-def highlight_term(id, term, text):
-    replaced_text = text.replace(
-        term, "\033[1;32;40m {term} \033[0;0m".format(term=term)
-    )
-    return "--- document {id}: {replaced}".format(id=id, replaced=replaced_text)
-
-
 def main():
     db = Database()
     index = InvertedIndex(db)
@@ -27,7 +20,7 @@ def main():
         for appearance in result[term]:
             # Belgium: { docId: 1, frequency: 1}
             document = db.get(appearance.docId)
-            print(highlight_term(appearance.docId, term, document["text"]))
+            print(appearance.docId, ": ", document["text"])
         print("-----------------------------")
 
 
