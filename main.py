860cc4878d533b86709373c2fe2f5ecfeb2f101f
Tanisha <tanisha031199@gmail.com>
2019-11-28 00:49:54 +0530

    addded

--- main.py
+++ main.py
@@ -44,36 +44,12 @@ def search():
                 document = db.get(appearance.docId)
                 print(appearance.docId, ": ", document["text"])
                 a.append((document["text"], appearance.frequency))
-        print(a, "-------------------------------------")
         a = sorted(a, key=lambda x: x[1])
-        print(a, "-------------------------------------")
         if a == []:
             a.append("Not Found")
         elif len(a) > 10:
             a = a[0:10]
         return render_template("final.html", ans=a)
-
-
-# def main():
-#     db = Database()
-#     index = InvertedIndex(db)
-#     document1 = {"id": "1", "text": "The big sharks of Belgium drink beer."}
-#     document2 = {
-#         "id": "2",
-#         "text": "Belgium has great beer. They drink beer all the time.",
-#     }
-#     index.index_document(document1)
-#     index.index_document(document2)
-
-#     search_term = input("Enter term(s) to search: ")
-#     result = index.lookup_query(search_term)
-
-#     for term in result.keys():
-#         for appearance in result[term]:
-#             # Belgium: { docId: 1, frequency: 1}
-#             document = db.get(appearance.docId)
-#             print(appearance.docId, ": ", document["text"])
-#         print("-----------------------------")
 
 
 if __name__ == "__main__":
